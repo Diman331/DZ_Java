6d579b1412092bcb5d9dd54ad2793904cb2857de
Diman331 <diman33133@gmail.com>
2023-06-09 23:04:50 +0700

    Initial commit

---
+++ Seminar_1/dz_1.java
@@ -0,0 +1,35 @@
+package Seminar_1;
+import java.util.Scanner;
+public class dz_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число n: ");
+        int n = scanner.nextInt();
+
+        int triangularNumber = calculateTriangularNumber(n);
+        System.out.println("n-ое треугольное число: " + triangularNumber);
+
+        int factorial = calculateFactorial(n);
+        System.out.println("n!: " + factorial);
+
+        scanner.close();
+    }
+
+    public static int calculateTriangularNumber(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int calculateFactorial(int n) {
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
